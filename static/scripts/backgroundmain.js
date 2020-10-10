ffaf9fbca6d7552129fc70bbbb7be2127a6cf855
iccupwin <iccupwin@gmail.com>
2020-10-10 16:05:50 +0600

    update

--- static/scripts/backgroundmain.js
+++ static/scripts/backgroundmain.js
@@ -22,7 +22,8 @@ particlesJS("particles-js", {
             "image": {
                 "src": "img/github.svg",
                 "width": 100,
-                "height": 100
+                "height": 100,
+                "color": "#000000"
             }
         },
         "opacity": {
